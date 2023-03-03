174ce070342b3e7cf7533be1d0d656a4f4bf95db
Настя Жулина <julina.nastia2017@yandex.ru>
2023-03-03 18:07:34 +0300

    специальные символы в капче

--- AssertAccounting/MainWindow.xaml.cs
+++ AssertAccounting/MainWindow.xaml.cs
@@ -130,7 +130,7 @@ namespace AssertAccounting
 
             for (int i = 0; i < 9; i++)
             {
-                switch (rand.Next(0, 3))
+                switch (rand.Next(0, 4))
                 {
                     case 0:
                         c = (char)rand.Next(49, 58);
@@ -141,29 +141,12 @@ namespace AssertAccounting
                     case 2:
                         c = (char)rand.Next(97, 123);
                         break;
+                    case 3:
+                        c = (char)rand.Next(32, 47);
+                        break;
                 }
                 result += c;
-
-                TextBlock tb = new TextBlock()
-                {
-                    Text = c.ToString(),
-                    Padding = new Thickness(i * 25 + 5, rand.Next(21), rand.Next(21), 10),
-                    FontSize = rand.Next(20, 26)
-                };
-
-                switch (rand.Next(0, 3))
-                {
-                    case 0:
-                        tb.FontStyle = FontStyles.Italic;
-                        break;
-                    case 1:
-                        tb.FontWeight = FontWeights.Bold;
-                        break;
-                    case 2:
-                        tb.FontStyle = FontStyles.Italic;
-                        tb.FontWeight = FontWeights.Bold;
-                        break;
-                }
+                       
             }
             MessageBox.Show(result, "Код", MessageBoxButton.OK);
             captha = result;
